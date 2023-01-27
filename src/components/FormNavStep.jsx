a717bce88cda85393b46fa86d744d8fb4107fae9
Kwongster <bryant.j.kwong@gmail.com>
2023-01-27 11:23:46 -0800

    navigation bar has elements dynamically generated

--- src/components/FormNavStep.jsx
+++ src/components/FormNavStep.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-export default function FormNavStep() {
+export default function FormNavStep({ stepNum, name }) {
     return (
         <div className="step-section">
             <div className="circle">
-                <span className="form-circle">1</span>
+                <span className="form-circle">{stepNum}</span>
             </div>
             <div className="step-text">
-                <h3 className="step">STEP 1</h3>
-                <h3 className="step-subtext">YOUR INFO</h3>
+                <h3 className="step">STEP {stepNum.toUpperCase()}</h3>
+                <h3 className="step-subtext">{name.toUpperCase()}</h3>
             </div>
         </div>
     )
