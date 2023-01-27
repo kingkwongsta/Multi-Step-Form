9ea873afb74c15939fd6920efb84ca31136d21f8
Kwongster <bryant.j.kwong@gmail.com>
2023-01-27 10:29:33 -0800

    configured components, moving to styling layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import FormNav from './components/FormNav'
+import Form from './components/Form'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h3>hello there</h3>
+      <div className="left-side">
+        <FormNav />
+      </div>
+      <div className="right-side">
+        <Form />
+      </div>
     </div>
   )
 }
