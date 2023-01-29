85ce1f51e7edee3372d5b3d57e3985415c850cdc
Kwongster <bryant.j.kwong@gmail.com>
2023-01-28 16:49:11 -0800

    completed form base html

--- src/components/FormPersonalInfo.jsx
+++ src/components/FormPersonalInfo.jsx
@@ -5,9 +5,18 @@ export default function FormPersonalInfo() {
     const [email, setEmail] = useState("")
     const [phone, setPhone] = useState("")
 
+    function handleName(event) {
+        setName(event.target.value)
+    }
     function handleEmail(event) {
         setEmail(event.target.value)
-        console.log(email)
+    }
+    function handlePhone(event) {
+        setPhone(event.target.value)
+    }
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(name, email, phone)
     }
 
     return (
@@ -15,9 +24,16 @@ export default function FormPersonalInfo() {
             <div className="form-personal">
                 <h2 className="form-personal-title">Personal info</h2>
                 <h4 className="form-personal-subtitle">Please provide your name, email address, and phone number.</h4>
-                <form action="" className="form-personal-section">
-                    <p className="form-personal-name-text">Name</p>
-                    <input type="text" value={email} onChange={handleEmail} className="form-personal-name-input" />
+                <form onSubmit={handleSubmit} className="form-personal-section">
+                    <p className="form-personal-form-text">Name</p>
+                    <input type="text" value={name} onChange={handleName} className="form-personal-input" />
+                    <p className="form-personal-form-text">Email Address</p>
+                    <input type="email" value={email} onChange={handleEmail} className="form-personal-input" />
+                    <p className="form-personal-form-text">Phone Number</p>
+                    <input type="tel" value={phone} onChange={handlePhone} className="form-personal-input" />
+                    <div>
+                        <button className="form-personal-submit">Next Step</button>
+                    </div>
                 </form>
             </div>
         </>
