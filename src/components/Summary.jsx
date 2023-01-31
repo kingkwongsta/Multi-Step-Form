b993e0a774e5da82edf10e6ef5542709f2d28e2e
Kwongster <bryant.j.kwong@gmail.com>
2023-01-30 21:10:41 -0800

    added base Summary form and Confirm button

---
+++ src/components/Summary.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Summary({ }) {
+
+
+    return (
+        <div className="summary-section">
+            <h2 className="form-title">Finishing up</h2>
+            <p className="form-subtext">Double-check everything looks OK before confirming.</p>
+        </div>
+    )
+}
