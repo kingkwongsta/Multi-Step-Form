171fdd94c0335895286c1cbaf87f9aa108a0a028
Kwongster <bryant.j.kwong@gmail.com>
2023-01-30 21:42:44 -0800

    setup state for plans

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -22,13 +22,25 @@ export default function Form({ step, handleNext, handleBack }) {
     function handlePhone(event) {
         setPhone(event.target.value)
     }
-    // ************************************
+    //****  PLAN & ADDON SECTION
+    const [plan, setPlan] = useState("Arcade")
+    const [addon, setAddon] = useState([])
+    function handlePlan(selection) {
+        setPlan(selection)
+        console.log("Plan has been set", plan)
+    }
+    function addAddon(selection) {
+        setAddon(prev => prev.push(selection))
+        console.log(addon)
+    }
+
+
 
     return (
         <>
             {step === 1 && <FormPersonalInfo name={name} email={email} phone={phone} handleNext={handleNext} handleName={handleName} handleEmail={handleEmail} handlePhone={handlePhone} />}
-            {step === 2 && <FormPlan handleNext={handleNext} handleBack={handleBack} />}
-            {step === 3 && <Addons handleNext={handleNext} handleBack={handleBack} />}
+            {step === 2 && <FormPlan handlePlan={handlePlan} handleNext={handleNext} handleBack={handleBack} />}
+            {step === 3 && <Addons addAddon={addAddon} handleNext={handleNext} handleBack={handleBack} />}
             {step === 4 && <Summary handleNext={handleNext} handleBack={handleBack} />}
             {step >= 2 && <Button className="form-back" onClick={handleBack}>Go Back</Button>}
             {step < 4 ? <Button className="form-next" onClick={handleNext}>Next Step</Button> : <Button className="form-confirm">Confirm</Button>}
--- src/components/PlanCard.jsx
+++ src/components/PlanCard.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
-export default function PlanCard({ name, monthly, icon }) {
+export default function PlanCard({ name, monthly, icon, handlePlan }) {
+    function handleSelection(selection) {
+        handlePlan(selection)
+    }
     return (
-        <div className="plan-card">
+        <div className="plan-card" onClick={() => handleSelection(name)}>
             <img src={icon} alt="" className="plan-card-icon" />
             <p className="plan-card-title">{name}</p>
             <p className="plan-card-price">${monthly}/mo</p>
