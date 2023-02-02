40c5c1526acb668f893f90cbedea3b56b1fdffc9
Kwongster <bryant.j.kwong@gmail.com>
2023-02-01 21:01:42 -0800

    moved addon data to own file in data folder

--- src/components/Addons.jsx
+++ src/components/Addons.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import Button from '@mui/material/Button';
 import AddonCard from "./AddonCard"
+import addonsData from "../data/addon"
 
 export default function Addons({ handleAddon, billing }) {
-    const addonsData = [{ title: "Online service", desc: "Access to multiplayer games", monthly: 1, yearly: 10 },
-    { title: "Larger storage", desc: "Extra 1TB of cloud save", monthly: 2, yearly: 20 },
-    { title: "Customizable Profile", desc: "Custom theme on your profile", monthly: 2, yearly: 20 }]
+
 
     function renderAddons() {
         return (
---
+++ src/data/addon.js
@@ -0,0 +1,3 @@
+export const addonsData = [{ title: "Online service", desc: "Access to multiplayer games", monthly: 1, yearly: 10 },
+{ title: "Larger storage", desc: "Extra 1TB of cloud save", monthly: 2, yearly: 20 },
+{ title: "Customizable Profile", desc: "Custom theme on your profile", monthly: 2, yearly: 20 }]
