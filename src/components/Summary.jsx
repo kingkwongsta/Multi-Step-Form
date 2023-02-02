89c296891102d55d8055c0585468481077b92ec8
Kwongster <bryant.j.kwong@gmail.com>
2023-02-01 23:31:42 -0800

    added a change button to modify billing

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -3,7 +3,7 @@ import planData from "../data/plan"
 import addonsData from "../data/addon"
 import SummaryAddon from "./SummaryAddon";
 
-export default function Summary({ addon, plan, billing }) {
+export default function Summary({ addon, plan, billing, handleBilling }) {
 
     // const temp = ["Online service", "Larger storage"]
 
@@ -32,8 +32,11 @@ export default function Summary({ addon, plan, billing }) {
             <h2 className="form-title">Finishing up</h2>
             <p className="form-subtext">Double-check everything looks OK before confirming.</p>
             <div className="plan-section">
-                <div className="plan-name">
-                    {plan} {billing ? <span>(Monthly)</span> : <span>(Yearly)</span>}
+                <div className="plan-section-and-button">
+                    <div className="plan-name">
+                        {plan} {billing ? <span>(Monthly)</span> : <span>(Yearly)</span>}
+                        <button onClick={handleBilling}>testing button</button>
+                    </div>
                 </div>
                 <div className="plan-price">
                     +${billing ? planMoPrice() : planYrPrice()}/{billing ? <span>mo</span> : <span>yr</span>}
