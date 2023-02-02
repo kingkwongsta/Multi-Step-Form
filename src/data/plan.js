b4d4a77470058fd83c0c2ad82ed6b3dc738d01bb
Kwongster <bryant.j.kwong@gmail.com>
2023-02-01 21:00:10 -0800

    moved plan data to it's own file in data folder

---
+++ src/data/plan.js
@@ -0,0 +1,8 @@
+import arcade_icon from "../assets/icon-arcade.svg"
+import advanced_icon from "../assets/icon-advanced.svg"
+import pro_icon from "../assets/icon-pro.svg"
+
+export const planData = [{ name: "Arcade", monthly: 9, yearly: 90, icon: arcade_icon },
+{ name: "Advanced", monthly: 12, yearly: 120, icon: advanced_icon },
+{ name: "Pro", monthly: 15, yearly: 150, icon: pro_icon }]
+
