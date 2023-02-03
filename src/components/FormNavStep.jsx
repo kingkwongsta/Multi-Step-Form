c2d7308e7fe95d77fc3b7d8cc61dd631400f3811
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-02-02 16:57:43 -0800

    styled navbar text

--- src/components/FormNavStep.jsx
+++ src/components/FormNavStep.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 export default function FormNavStep({ stepNum, name }) {
-    return (
-        <div className="step-section">
-            <div className="circle">
-                <span className="form-circle">{stepNum}</span>
-            </div>
-            <div className="step-text">
-                <h3 className="step">STEP {stepNum.toUpperCase()}</h3>
-                <h3 className="step-subtext">{name.toUpperCase()}</h3>
-            </div>
-        </div>
-    )
+  return (
+    <div className="step-section">
+      <div className="circle">
+        <p className="form-circle">{stepNum}</p>
+      </div>
+      <div className="step-text-section">
+        <h3 className="step-text">STEP {stepNum.toUpperCase()}</h3>
+        <h3 className="step-subtext">{name.toUpperCase()}</h3>
+      </div>
+    </div>
+  );
 }
