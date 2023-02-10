7d568a5483df969ab8585d264d0d7ce0948d7018
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-02-09 21:56:19 -0800

    comments for different state in App.js

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -8,20 +8,6 @@ import Complete from "./Complete";
 // MUI
 import Button from "@mui/material/Button";
 
-import { createTheme } from "@mui/material/styles";
-import { green, purple } from "@mui/material/colors";
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: purple[500],
-    },
-    secondary: {
-      main: green[500],
-    },
-  },
-});
-
 export default function Form({ step, handleNext, handleBack }) {
   //****  PERSONAL INFORMATION SECTION
   const [name, setName] = useState("");
