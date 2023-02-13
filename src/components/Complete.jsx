3ebad5ab984ddf1cb63be9b4cd15d4b7aad95317
Bryant Kwong <bryant.j.kwong@gmail.com>
2023-02-12 16:39:09 -0800

    styling for complete page

--- src/components/Complete.jsx
+++ src/components/Complete.jsx
@@ -4,8 +4,8 @@ import thank_you_image from "../assets/icon-thank-you.svg";
 export default function Complete() {
   return (
     <div className="thank-you-section">
-      <img src={thank_you_image} alt="" />
-      <h2 className="thank-you-title">Thank You!</h2>
+      <img className="thank-you-icon" src={thank_you_image} alt="" />
+      <h2 className="thank-you-title">Thank you!</h2>
       <p className="thank-you-desc">
         Thanks for confirming your subscription! We hope you have fun using our
         platform. If you ever need support, please feel free to email us at
